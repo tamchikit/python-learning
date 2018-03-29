df7af2c81670553c9ff844638b1cf408a19ab505
tamchikit <tamchikit@163.com>
2018-03-29 22:08:35 +0800

    day-07

--- awesome-python3-webapp/www/app.py
+++ awesome-python3-webapp/www/app.py
@@ -1,3 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'Tamchikit'
+
+'''
+async web application.
+'''
+
 import logging; logging.basicConfig(level=logging.INFO)
 
 import asyncio, os, json, time
@@ -12,22 +21,22 @@ from coroweb import add_routes, add_static
 def init_jinja2(app, **kw):
     logging.info('init jinja2...')
     options = dict(
-                   autoescape = kw.get('autoescape', True),
-                   block_start_string = kw.get('block_start_string', '{%'),
-                   block_end_string = kw.get('block_end_string', '%}'),
-                   variable_start_string = kw.get('variable_start_string', '{{'),
-                   variable_end_string = kw.get('variable_end_string', '}}'),
-                   auto_reload = kw.get('auto_reload', True)
-                   )
-                   path = kw.get('path', None)
-                   if path is None:
-                       path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
-                   logging.info('set jinja2 template path: %s' % path)
-                   env = Environment(loader=FileSystemLoader(path), **options)
-filters = kw.get('filters', None)
-if filters is not None:
-    for name, f in filters.items():
-        env.filters[name] = f
+        autoescape = kw.get('autoescape', True),
+        block_start_string = kw.get('block_start_string', '{%'),
+        block_end_string = kw.get('block_end_string', '%}'),
+        variable_start_string = kw.get('variable_start_string', '{{'),
+        variable_end_string = kw.get('variable_end_string', '}}'),
+        auto_reload = kw.get('auto_reload', True)
+    )
+    path = kw.get('path', None)
+    if path is None:
+        path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
+    logging.info('set jinja2 template path: %s' % path)
+    env = Environment(loader=FileSystemLoader(path), **options)
+    filters = kw.get('filters', None)
+    if filters is not None:
+        for name, f in filters.items():
+            env.filters[name] = f
     app['__templating__'] = env
 
 async def logger_factory(app, handler):
@@ -101,16 +110,16 @@ def datetime_filter(t):
     return u'%s年%s月%s日' % (dt.year, dt.month, dt.day)
 
 async def init(loop):
-    await orm.create_pool(loop=loop, host='127.0.0.1', port=3306, user='www', password='www', db='awesome')
+    await orm.create_pool(loop=loop, host='127.0.0.1', port=3306, user='www-data', password='www-data', db='awesome')
     app = web.Application(loop=loop, middlewares=[
-                                                  logger_factory, response_factory
-                                                  ])
-                                                  init_jinja2(app, filters=dict(datetime=datetime_filter))
-                                                  add_routes(app, 'handlers')
-                                                  add_static(app)
-                                                  srv = await loop.create_server(app.make_handler(), '127.0.0.1', 9000)
-                                                  logging.info('server started at http://127.0.0.1:9000...')
-                                                  return srv
+        logger_factory, response_factory
+    ])
+    init_jinja2(app, filters=dict(datetime=datetime_filter))
+    add_routes(app, 'handlers')
+    add_static(app)
+    srv = await loop.create_server(app.make_handler(), '127.0.0.1', 9000)
+    logging.info('server started at http://127.0.0.1:9000...')
+    return srv
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(init(loop))
