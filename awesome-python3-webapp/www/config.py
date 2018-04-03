789084e6b85c79cd2d8dee98b2955625c2eecf58
tamchikit <tamchikit@163.com>
2018-04-03 21:35:00 +0800

    day-09

--- awesome-python3-webapp/www/config.py
+++ awesome-python3-webapp/www/config.py
@@ -1,24 +1,31 @@
-#!/usr/bin/env python
-# config.py
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+Configuration
+'''
+
+__author__ = 'Michael Liao'
+
 import config_default
 
 class Dict(dict):
     '''
-        Simple dict but support access as x.y style.
-        '''
+    Simple dict but support access as x.y style.
+    '''
     def __init__(self, names=(), values=(), **kw):
         super(Dict, self).__init__(**kw)
         for k, v in zip(names, values):
             self[k] = v
 
-def __getattr__(self, key):
-    try:
-        return self[key]
+    def __getattr__(self, key):
+        try:
+            return self[key]
         except KeyError:
             raise AttributeError(r"'Dict' object has no attribute '%s'" % key)
                 
-                def __setattr__(self, key, value):
-                    self[key] = value
+    def __setattr__(self, key, value):
+        self[key] = value
 
 def merge(defaults, override):
     r = {}
@@ -47,3 +54,4 @@ except ImportError:
     pass
 
 configs = toDict(configs)
+
--- awesome-python3-webapp/www/coroweb.py
+++ awesome-python3-webapp/www/coroweb.py
@@ -13,8 +13,8 @@ from apis import APIError
 
 def get(path):
     '''
-        Define decorator @get('/path')
-        '''
+    Define decorator @get('/path')
+    '''
     def decorator(func):
         @functools.wraps(func)
         def wrapper(*args, **kw):
@@ -88,7 +88,8 @@ class RequestHandler(object):
         self._named_kw_args = get_named_kw_args(fn)
         self._required_kw_args = get_required_kw_args(fn)
     
-    async def __call__(self, request):
+    @asyncio.coroutine
+    def __call__(self, request):
         kw = None
         if self._has_var_kw_arg or self._has_named_kw_args or self._required_kw_args:
             if request.method == 'POST':
@@ -96,12 +97,12 @@ class RequestHandler(object):
                     return web.HTTPBadRequest('Missing Content-Type.')
                 ct = request.content_type.lower()
                 if ct.startswith('application/json'):
-                    params = await request.json()
+                    params = yield from request.json()
                     if not isinstance(params, dict):
                         return web.HTTPBadRequest('JSON body must be object.')
                     kw = params
                 elif ct.startswith('application/x-www-form-urlencoded') or ct.startswith('multipart/form-data'):
-                    params = await request.post()
+                    params = yield from request.post()
                     kw = dict(**params)
                 else:
                     return web.HTTPBadRequest('Unsupported Content-Type: %s' % request.content_type)
@@ -135,7 +136,7 @@ class RequestHandler(object):
                     return web.HTTPBadRequest('Missing argument: %s' % name)
         logging.info('call with args: %s' % str(kw))
         try:
-            r = await self._func(**kw)
+            r = yield from self._func(**kw)
             return r
         except APIError as e:
             return dict(error=e.error, data=e.data, message=e.message)
@@ -171,3 +172,4 @@ def add_routes(app, module_name):
             path = getattr(fn, '__route__', None)
             if method and path:
                 add_route(app, fn)
+
